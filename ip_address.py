dd0938ba2b55c5b645527fe768d8e29448bde139
billycraig2 <91530897+billycraig2@users.noreply.github.com>
2024-11-20 16:40:27 +0530

    Update ip_address.py

--- ip_address.py
+++ ip_address.py
@@ -1,5 +1,6 @@
 import re
 
+# TASK 3 - Take an array of strings as input and segregate them to ipv4 and ipv6 using regex.
 # uses regex to print if email is valid
 def ip_type(ips):
     # checks string of ips with regex
